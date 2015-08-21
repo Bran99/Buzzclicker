00d09de2df18e15e705dd0afd1dbf72f7cfc2234
Brandon Goodman <bran99@gmail.com>
2015-08-21 17:00:52 -0400

    server.js pretty much done

--- server.js
+++ server.js
@@ -12,3 +12,37 @@ var express = require('express'),
 
 var PORT = process.env.PORT || 3000;
 var MONGOURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/whale-drop';
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('short'));
+app.use(express.static('./public'));
+
+app.use(expressLayouts);
+app.use(session({
+  secret : process.env.SESSION_SECRET,
+  resave : false,
+  saveUninitialized : false
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+var usersController = require('./controllers/users.js');
+app.use('/', usersController)
+
+mongoose.connect(MONGOURI);
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('DATABASE ERRORS!');
+  console.log(err);
+});
+
+db.once('open', function () {
+  console.log('DATABASE IS RUNNING');
+  app.listen(PORT, function () {
+    console.log('SERVER IS UP ON PORT', PORT);
+  });
+});
