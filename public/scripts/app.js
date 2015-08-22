9d65ca57159ec0127025de35e0811d5f3081e76f
Brandon Goodman <bran99@gmail.com>
2015-08-22 14:53:37 -0400

    Added a beer counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,12 +4,13 @@ app.controller('buzzClickerController', ["$http", "$scope", function($http, $sco
   this.barWidth = 0;
   this.upgrades = 0;
   this.level    = 0;
+  this.beers    = 0;
   this.beerType;
 
   var controller = this;
 
   ///////////////////////////////////////////
-  // TICK DRUNK METER DOWN EVERY 2 SECONDS //
+  //// TICK DRUNK METER DOWN EVERY SECOND ///
   ///////////////////////////////////////////
   var interval = setInterval(function () {
     $http.post('/tick', { level : controller.level })
@@ -23,7 +24,7 @@ app.controller('buzzClickerController', ["$http", "$scope", function($http, $sco
   // TICK DRUNK METER UP WHEN BEER CLICKED //
   ///////////////////////////////////////////
   this.drink = function () {
-    // $('.arm').toggleClass('')
+    controller.beers++;
     $http.post('/drink', { level : controller.level })
          .success(function (data) {
            controller.barWidth = data.drunkity + '%';
